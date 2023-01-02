b5bf8f3b1619a90536fdaaa77a32296004201a2e
Breno Mendes <b.mendesbernardo2020@gmail.com>
2023-01-02 08:03:48 -0300

    localizador-de-cep

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
+import api from "./services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    //01310930/json
+
+    if (input === "") {
+      alert("Preencha algum CEP!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      console.log(response.data);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("Ops! erro ao buscar...");
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite o seu  CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logadouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
